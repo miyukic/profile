64b59737eaa99927032891cc42b55d91ed06e06f
miyukic <m.yukic10@gmail.com>
2021-12-27 18:42:47 +0900

    typescript導入

--- src/Icon.tsx
+++ src/Icon.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import IconPng from './icon.png';
 import { Box } from '@chakra-ui/react'
 
-const Icon = (props) => {
+const Icon = (props: any) => {
     let height = props.height;
     let width = props.width;
     return (
