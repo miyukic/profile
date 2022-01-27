bf13117635c73a50c1075d3664f78991a7bf48c4
miyukic <m.yukic10@gmail.com>
2022-01-27 23:27:09 +0900

    update

--- src/Icon.tsx
+++ src/Icon.tsx
@@ -7,15 +7,19 @@ interface IconProps {
     readonly width?: number;
     readonly height?: number;
     readonly image: string;
+    readonly isCenter: boolean;
 }
 
 
-const Icon: React.FC<IconProps> = ({width=100, height=100, image}: IconProps) => {
+const Icon: React.FC<IconProps> = ({width=100, height=100, image, isCenter=false}: IconProps) => {
     const [size, setSize] = useState<[number, number]>();
-
+    let center: string | undefined = undefined;
+    if (isCenter === true) {
+        center = "auto";
+    }
     return (
 
-        <Circle mx={'auto'}  width={width} height={height} bg={'gray.100'}>
+        <Circle mx={center}  width={width} height={height} bg={'gray.100'}>
             <Image mx={3} padding={3} src={image} alt="miyuki" borderRadius='full' fallbackSrc='https://via.placeholder.com/150' align='center'></Image>
         </Circle>
     )
