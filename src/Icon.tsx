6d86dffc638266f3e11bd9cd9713d0690dc68207
miyukic <m.yukic10@gmail.com>
2022-02-09 23:54:39 +0900

    update

--- src/Icon.tsx
+++ src/Icon.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Box, Circle, Image } from '@chakra-ui/react'
+import React, { useEffect, useRef } from 'react';
+import { Avatar, Box, Circle, Image } from '@chakra-ui/react'
 import { getWidth } from './index'
 import { useState } from 'react';
 
@@ -17,13 +17,23 @@ const Icon: React.FC<IconProps> = ({width=100, height=100, image, isCenter=false
     if (isCenter === true) {
         center = "auto";
     }
+
+    const imgRef = useRef(null);
+
+    useEffect(
+        () => {
+            if (imgRef == null || imgRef == undefined) console.log("imgRef は Null または、Undefined です。");
+            //console.log("Icon " + imgRef.current.getBoundingClientRect());
+        }
+    );
+
     return (
 
         <Box align={"center"} width={width}>
-            <Circle mx={center}  width={width} height={height} bg={'gray.100'} margin={1}>
-                <Image mx={3} padding={3} src={image} alt="miyuki" borderRadius='full' fallbackSrc='https://via.placeholder.com/150' align='center'>
+            <Circle mx={center} width={width} height={height} bg={'gray.100'} margin={1}>
+                <Avatar ref={imgRef} name={"miyukiumoo"} mx={3} padding={3} src={image} alt="miyuki" borderRadius='full' fallbackSrc='https://via.placeholder.com/150' align='center' onClick={() => console.log(imgRef.current)}>
                     {/* <Box minwidth={width} minheight={height} bgColor={'AppWorkspace'}>a</Box> */}
-                </Image>
+                </Avatar>
             </Circle>
             <Box fontFamily={'sans-serif'} fontSize={"3xl"} color={'gray.500'}>Miyukiumoo</Box>
         </Box>
